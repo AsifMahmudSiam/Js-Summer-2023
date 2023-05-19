62275b032f7b0ab03763d8226027c1ab8efc5ff5
Asif <asifmahmudsiam@gmail.com>
2023-05-19 19:54:55 -0400

    update

--- Modules/Operator/ConditionalOperator2.js
+++ Modules/Operator/ConditionalOperator2.js
@@ -1,7 +1,7 @@
 //Declear a variable 
 let price = 15;
 //using tarnary operator
-let result =(price<20)? "The product is on sale": "The product is not on sale";
+let result =(price<=20)? "The product is on sale": "The product is not on sale";
 // print the result
 console.log(result);
 
